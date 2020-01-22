140e225fefae3b4c8d5a47109e9b29753ef43be4
SirGrordbort <toby.messerli@gmail.com>
2020-01-22 15:00:23 +1300

    finished mapping except for the weird coordinate system

--- Map_eq_locations.py
+++ Map_eq_locations.py
@@ -1,6 +1,18 @@
 # for mapping events from the obspy catalog file into arcGIS
 from obspy import read_events
-
-catalog = read_events("catalog")
+import arcpy
+earthquakes = arcpy.GetParameter(0)
+catalog = read_events("C:/Users/messertoby/PycharmProjects/Match-filter-detection/catalog")
+points = []
 for event in catalog:
-    print(event.Origin)
+    origin = event.preferred_origin()
+    point = arcpy.Point(origin.latitude, origin.longitude)
+    points.append(arcpy.PointGeometry(point))
+
+arcpy.CopyFeatures_management(points,earthquakes)
+arcpy.AddField_management(earthquakes, "Describe", "STRING")
+eq_cursor = arcpy.da.UpdateCursor(earthquakes, ("Describe",))
+
+for row, event in zip(eq_cursor, catalog):
+    row[0] = event.short_str()
+    eq_cursor.updateRow(row)
