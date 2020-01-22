415cbcf96b829cb9daad9f6791255e419da15cb5
SirGrordbort <toby.messerli@gmail.com>
2020-01-23 12:47:35 +1300

    events in the catalog now map correctly to the world map

--- Map_eq_locations.py
+++ Map_eq_locations.py
@@ -1,18 +1,34 @@
 # for mapping events from the obspy catalog file into arcGIS
 from obspy import read_events
 import arcpy
-earthquakes = arcpy.GetParameter(0)
-catalog = read_events("C:/Users/messertoby/PycharmProjects/Match-filter-detection/catalog")
+
+# The file location of the obspy event catalog
+input_file = arcpy.GetParameterAsText(0)
+
+# The output feature class
+earthquakes = arcpy.GetParameter(1)
+
+catalog = read_events(input_file)
 points = []
+# Makes the initial feature class from the coordinates of the events stored in the catalog
 for event in catalog:
     origin = event.preferred_origin()
-    point = arcpy.Point(origin.latitude, origin.longitude)
+    point = arcpy.Point(origin.longitude, origin.latitude)
     points.append(arcpy.PointGeometry(point))
-
 arcpy.CopyFeatures_management(points,earthquakes)
-arcpy.AddField_management(earthquakes, "Describe", "STRING")
-eq_cursor = arcpy.da.UpdateCursor(earthquakes, ("Describe",))
 
+# Adds useful fields to the output feature class
+arcpy.AddField_management(earthquakes, "Describe", "STRING")
+arcpy.AddField_management(earthquakes, "Magnitude", "Double")
+arcpy.AddField_management(earthquakes, "Depth", "Double")
+arcpy.AddField_management(earthquakes, "Time", "String")
+eq_cursor = arcpy.da.UpdateCursor(earthquakes, ("Describe","Magnitude","Depth", "Time",))
 for row, event in zip(eq_cursor, catalog):
     row[0] = event.short_str()
+    row[1] = event.preferred_magnitude().mag
+    row[2] = event.preferred_origin().depth
+    row[3] = event.preferred_origin().time.format_iris_web_service()
     eq_cursor.updateRow(row)
+
+# Adds the correct coordinate system to the output feature class
+arcpy.DefineProjection_management(earthquakes, "wgs84.prj")
